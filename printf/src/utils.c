f9afbffc0f777fa88558e02612e2f3f25ee8a659
Muhammad Anas Majeed Rafique <mumajeed@car1s4.42barcelona.com>
2024-12-16 12:59:04 +0100

    signature

--- printf/src/utils.c
+++ printf/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:23:16 by mumajeed          #+#    #+#             */
-/*   Updated: 2024/12/16 12:29:24 by mumajeed         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:50:31 by mumajeed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,25 @@ int	ft_putchar(char c)
 	return (write(1, &c, 1));
 }
 
-int ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
 	int	len;
 
 	len = 0;
 	if (!s)
 		return (ft_putstr("(null)"));
+	while (s[len])
+		ft_putchar(s[len++]);
+	return (len);
+}
+
+int	ft_putnbr(int n)
+{
+	char	*num;
+	int		len;
+
+	num = ft_itoa(n);
+	len = ft_putstr(num);
+	free(num);
+	return (len);
 }
